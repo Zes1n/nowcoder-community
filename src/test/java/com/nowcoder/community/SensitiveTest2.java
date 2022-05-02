5837f3d900d0e3b72df2e793f697dc9af2cc5b12
wuzexin <4066468@qq.com>
2022-05-02 11:43:06 +0800

    修改springboot版本

--- src/test/java/com/nowcoder/community/SensitiveTest2.java
+++ src/test/java/com/nowcoder/community/SensitiveTest2.java
@@ -1,9 +1,12 @@
 package com.nowcoder.community;
 
 import com.nowcoder.community.util.SensitiveFilter;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * TODO
@@ -11,7 +14,9 @@ import org.springframework.boot.test.context.SpringBootTest;
  * @author wuzexin
  * @created 2022/4/22 08:04
  */
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
 public class SensitiveTest2 {
 
     @Autowired
