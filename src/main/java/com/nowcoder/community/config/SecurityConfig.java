ffcff40a6de9b651a3823664b8d6bfdda9a21046
wuzexin <4066468@qq.com>
2022-05-04 11:55:18 +0800

    权限控制

---
+++ src/main/java/com/nowcoder/community/config/SecurityConfig.java
@@ -0,0 +1,108 @@
+package com.nowcoder.community.config;
+
+import com.nowcoder.community.util.CommunityConstant;
+import com.nowcoder.community.util.CommunityUtil;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * security配置类
+ *
+ * @author wuzexin
+ * @created 2022/5/4 09:53
+ */
+
+
+/**
+ * 继承 WebSecurityConfigurerAdapter
+ * 重写三个configure方法
+ * 1、参数为WebSecurity的方法，用于忽略对静态资源的拦截
+ * 2、参数为AuthenticatioMangerBuilder的方法，用于认证，由于原系统已有认证方式，就不重写了，直接绕过
+ * 3、参数为HttpSecurity的方法，用于授权
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter implements CommunityConstant {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // 拦截静态资源
+        web.ignoring().antMatchers("/resources/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 授权
+        http.authorizeRequests()
+                .antMatchers(
+                        "/user/setting",
+                        "/user/upload",
+                        "/discuss/add",
+                        "/comment/add/**",
+                        "/letter/**",
+                        "/notice/**",
+                        "/like",
+                        "/follow",
+                        "/unfollow"
+                )
+                .hasAnyAuthority(
+                        AUTHORITY_USER,
+                        AUTHORITY_ADMIN,
+                        AUTHORITY_MODERATOR
+                )
+                .anyRequest().permitAll()
+                // 关闭csrf
+                .and().csrf().disable();
+
+        // 权限不够时的处理
+        http.exceptionHandling()
+                .authenticationEntryPoint(new AuthenticationEntryPoint() { /*没登录时处理方式*/
+                    @Override
+                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
+                        String xRequestedWith = request.getHeader("x-requested-with");
+                        if ("XMLHttpRequest".equals(xRequestedWith)) {
+                            // 如果返回"XMLHttpRequest"， 就是异步请求，需要给浏览器返回Json
+                            response.setContentType("application/plain;charset=utf-8");
+                            PrintWriter writer = response.getWriter();
+                            writer.write(CommunityUtil.getJSONString(403,"你还没有登录！"));
+                        } else {
+                            // 如果不是异步请求，重定向到登录页面
+                            response.sendRedirect(request.getContextPath() + "/login");
+                        }
+                    }
+                })
+                .accessDeniedHandler(new AccessDeniedHandler() {    /*权限不够时处理方式*/
+                    @Override
+                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException e) throws IOException, ServletException {
+                        String xRequestedWith = request.getHeader("x-requested-with");
+                        if ("XMLHttpRequest".equals(xRequestedWith)) {
+                            // 如果返回"XMLHttpRequest"， 就是异步请求，需要给浏览器返回Json
+                            response.setContentType("application/plain;charset=utf-8");
+                            PrintWriter writer = response.getWriter();
+                            writer.write(CommunityUtil.getJSONString(403,"你没有访问此功能的权限！"));
+                        } else {
+                            // 如果不是异步请求，重定向到登录页面
+                            response.sendRedirect(request.getContextPath() + "/denied");
+                        }
+                    }
+                });
+
+        // Security底层默认会拦截/logout请求，进行退出处理。
+        // 覆盖它默认的逻辑才能执行我们自己的退出代码
+        http.logout().logoutUrl("/securitylogout"); // 给他一个不存在的路径，它就没法处理了
+
+    }
+
+}
--- src/main/java/com/nowcoder/community/config/WebMvcConfig.java
+++ src/main/java/com/nowcoder/community/config/WebMvcConfig.java
@@ -25,8 +25,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginTicketInteceptor loginTicketInteceptor;
 
-    @Autowired
-    private LoginRequiredInterceptor loginRequiredInterceptor;
+//    @Autowired
+//    private LoginRequiredInterceptor loginRequiredInterceptor;
 
     @Autowired
     private MessageInterceptor messageInterceptor;
@@ -40,8 +40,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(loginTicketInteceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
 
-        registry.addInterceptor(loginRequiredInterceptor)
-                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+//        registry.addInterceptor(loginRequiredInterceptor)
+//                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
 
         registry.addInterceptor(messageInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
